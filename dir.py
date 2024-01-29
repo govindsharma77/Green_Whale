97d3a9b26756932dfeb99f6ec142d4f56006ef1d
govindsharma77 <gsharma73@myseneca.ca>
2024-01-29 16:54:27 -0500

    Added files to github that I was working on locally

---
+++ dir.py
@@ -0,0 +1,40 @@
+#Govind Sharma
+#25-March-2023
+#deer in headlights program for ADHD object perminance
+
+from menu import breakfast, meal, finger_food
+import menu
+import random
+import time 
+
+def select_food():
+    category = input("\n\nWhat category of food do you want? \nBreakfast\nMeal\nFinger Food\n>")
+
+    if category == "breakfast" or category == "b":
+        breakfast = random.choice(list(menu.breakfast.keys()))
+        #time.sleep(1.5)
+        print("Your breakfast is", breakfast)
+    elif category == "meal" or category == "m" or category == "lunch" or category == "l" or category == "dinner" or category == "d":
+        meal = random.choice(list(menu.meal.keys()))
+        #time.sleep(1.5)
+        print("Your meal is", meal)
+
+    elif category == "snacckies" or category == "snacks" or category == "finger foods" or category == "finger food" or category == "f" or category == "s":
+        finger_food = random.choice(list(menu.finger_food.keys()))
+        #time.sleep(1.5)
+        print("Your snacckies are", finger_food)
+
+    elif category == "quit" or category == "q":
+        exit()
+    else:
+        print("go back to elementary school")
+        
+while True:
+    select_food()
+    time.sleep(2)
+    
+    
+    
+    
+
+
---
+++ menu.py
@@ -0,0 +1,20 @@
+
+breakfast = {
+    "Chidve": "flattened rice, onion, potatoes, peanuts",
+    "Batman juice": "protein powder, spuralina, oat milk, chia seeds",
+    "Pathra": "pathra"
+}
+
+meal = {
+    "Mediterranean Sando": "bread, hummus, pesto, cucumbers, capsicum",
+    "Street Paneer Sando": "bread, paneer, onion, capsicum, chipotle aioli",
+    "Taiwan Tofu": "tofu, soy sauce, ketchup"
+}
+
+finger_food = {
+    "potato chips": "potatoes, oil, salt",
+    "tortilla chips and cheese": "tortilla chips, cheese, salsa",
+    "red velvet cookies": "made good, mg",
+    "popcorn": "kala loon, popcorn",
+}
+
